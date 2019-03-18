32e339e1740fda5077cc8627dbe559598e49b99c
Jill Heske <jill.heske@udacity.com>
2019-03-18 19:15:43 -0400

    Step.04-Exercise-Display-SleepQuality-List

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -25,6 +25,7 @@ import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
 import com.example.android.trackmysleepquality.database.SleepNight
 
+// TODO (06) Change RecyclerView.Adapter’s parameter to <SleepNightAdapter.ViewHolder>.
 class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
 
     var data = listOf<SleepNight>()
@@ -35,6 +36,7 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
 
     override fun getItemCount() = data.size
 
+    // TODO (08) Change onBindViewHolder’s holder parameter type to ViewHolder, and update views.
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
         val item = data[position]
         holder.textView.text = item.sleepQuality.toString()
@@ -49,10 +51,19 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
 
+        // TODO (07) Update view to inflate list_item_sleep_night, and change
+        // return type to ViewHolder.
+
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater
                 .inflate(R.layout.text_item_view, parent, false) as TextView
 
         return TextItemViewHolder(view)
     }
+
+    // TODO (04) Create a ViewHolder class that extends RecyclerView.ViewHolder.
+
+    // TODO (05) Inside the ViewHolder, use findViewById() to create properties for sleepLength,
+    // quality, and qualityImage.
+
 }
