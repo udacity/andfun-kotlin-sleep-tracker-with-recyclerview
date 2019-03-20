76fbf708f099121402eaabc32d6fd6900cdcd77c
Jill Heske <jill.heske@udacity.com>
2019-03-20 10:26:17 -0400

    Step.10-Solution-Replace-LinearLayout-with-GridLayout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -97,7 +98,8 @@ class SleepTrackerFragment : Fragment() {
             }
         })
 
-        // TODO (02) Create a GridLayoutManager spanning 3 columns and bind it to the RecyclerView.
+        val manager = GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager = manager
 
         val adapter = SleepNightAdapter()
         binding.sleepList.adapter = adapter
