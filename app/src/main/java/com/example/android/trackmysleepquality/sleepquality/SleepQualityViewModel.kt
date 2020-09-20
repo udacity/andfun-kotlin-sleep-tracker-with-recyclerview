6a4c23c367e851d81c550f171e210ae3262a5ca6
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-09-21 00:53:17 +0530

    Remove deprecated issues

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -19,6 +19,7 @@ package com.example.android.trackmysleepquality.sleepquality
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import kotlinx.coroutines.*
 
@@ -31,7 +32,6 @@ class SleepQualityViewModel(
         private val sleepNightKey: Long = 0L,
         val database: SleepDatabaseDao) : ViewModel() {
 
-    /** Coroutine setup variables */
 
     /**
      * viewModelJob allows us to cancel all coroutines started by this ViewModel.
@@ -46,9 +46,7 @@ class SleepQualityViewModel(
      *
      * By default, all coroutines started in uiScope will launch in [Dispatchers.Main] which is
      * the main thread on Android. This is a sensible default because most coroutines started by
-     * a [ViewModel] update the UI after performing some processing.
      */
-    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     /**
      * Variable that tells the fragment whether it should navigate to [SleepTrackerFragment].
@@ -65,14 +63,8 @@ class SleepQualityViewModel(
         get() = _navigateToSleepTracker
 
     /**
-     * Cancels all coroutines when the ViewModel is cleared, to cleanup any pending work.
      *
-     * onCleared() gets called when the ViewModel is destroyed.
      */
-    override fun onCleared() {
-        super.onCleared()
-        viewModelJob.cancel()
-    }
 
     /**
      * Call this immediately after navigating to [SleepTrackerFragment]
@@ -87,14 +79,12 @@ class SleepQualityViewModel(
      * Then navigates back to the SleepTrackerFragment.
      */
     fun onSetSleepQuality(quality: Int) {
-        uiScope.launch {
+        viewModelScope.launch {
             // IO is a thread pool for running operations that access the disk, such as
             // our Room database.
-            withContext(Dispatchers.IO) {
-                val tonight = database.get(sleepNightKey) ?: return@withContext
+                val tonight = database.get(sleepNightKey) ?: return@launch
                 tonight.sleepQuality = quality
                 database.update(tonight)
-            }
 
             // Setting this state variable to true will alert the observer and trigger navigation.
             _navigateToSleepTracker.value = true
