370fc870a7a3eca510cd8df2cca1471242473b76
Diraj H S <dirajhsdj@gmail.com>
2024-06-14 12:05:32 +0530

    Update dependencies and fixed related issues
    Signed-off-by: Diraj H S <dirajhsdj@gmail.com>

--- app/src/main/java/com/example/android/trackmysleepquality/sleepdetail/SleepDetailViewModelFactory.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepdetail/SleepDetailViewModelFactory.kt
@@ -29,7 +29,7 @@ class SleepDetailViewModelFactory(
         private val sleepNightKey: Long,
         private val dataSource: SleepDatabaseDao) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(SleepDetailViewModel::class.java)) {
             return SleepDetailViewModel(sleepNightKey, dataSource) as T
         }
